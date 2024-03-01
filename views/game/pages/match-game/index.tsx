99bff2da875362221c9b09bb08051489cdbddf79
BuiMinhHieu <hieu.bm@softworld.jp>
2024-03-01 15:20:01 +0700

    add match game version 2

--- views/game/pages/match-game/index.tsx
+++ views/game/pages/match-game/index.tsx
@@ -5,7 +5,7 @@ function MatchGamePage() {
 	return (
 		<div className="relative bg-gray-100 w-screen h-screen">
 			<GameComunicator>
-				<iframe title="match-game" src="/sw-match-game/index.html" id="match-game" className="w-full h-full" />
+				<iframe title="match-game" src="/sw-match-game-v1/index.html" id="match-game" className="w-full h-full" />
 			</GameComunicator>
 		</div>
 	)
