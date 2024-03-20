1818bcfc86afd29b0f4d8eb0bd78591278c74475
BuiMinhHieu <hieu.bm@softworld.jp>
2024-03-20 13:08:03 +0700

    change default skin

--- views/game/pages/character-game/providers/initial-state/index.ts
+++ views/game/pages/character-game/providers/initial-state/index.ts
@@ -2,8 +2,8 @@ import { ICharacterClothes } from "../type";
 
 export const characterClothesState: ICharacterClothes = {
   costume: {
-    shirt: { url: '/assets/game/character-game/clothes/shirt/shirt-01.png' },
-    hat: { url: '/assets/game/character-game/clothes/hat/hat-01.png' },
-    wing: { url: '/assets/game/character-game/clothes/wing/wing-01.png' },
+    shirt: { url: '/assets/game/character-game/clothes/skin-empty.png' },
+    hat: { url: '/assets/game/character-game/clothes/skin-empty.png' },
+    wing: { url: '/assets/game/character-game/clothes/skin-empty.png' },
   }
 }
